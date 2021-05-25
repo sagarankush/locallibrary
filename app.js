fe1b8462746c34e3929927d2d2b97eda2affd20a
Ankush <ankushsagar30@gmail.com>
2021-05-25 11:19:56 +0530

    Setup Mongoose and populate DB

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://admin:admin@cluster0.utgar.mongodb.net/local_library?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
